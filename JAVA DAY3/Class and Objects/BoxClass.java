9e68f2c4061299c852d0cb3c39c18be4474581cd
Aarthi L <54667558+aarthi03@users.noreply.github.com>
2020-05-29 23:43:07 +0530

    Add files via upload

---
+++ JAVA DAY3/Class and Objects/BoxClass.java
@@ -0,0 +1,23 @@
+public class BoxClass {
+    double h,w,d;
+
+     BoxClass(double width,double height,double depth)
+    {
+        h=height;
+        w=width;
+        d=depth;
+    }
+    double volume()
+    { double v;
+    v=h*w*d;
+    return v;
+    }
+   
+    public static void main(String[] args) {
+       
+    BoxClass bc = new BoxClass(8.5,80.3,9.6);
+    System.out.println(bc.volume());
+
+    }
+
+}
