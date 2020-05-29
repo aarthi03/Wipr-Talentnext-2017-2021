a53fa76cab16fc70f559231d081ce0a0f037ff40
Aarthi L <54667558+aarthi03@users.noreply.github.com>
2020-05-30 00:09:25 +0530

    Add files via upload

---
+++ JAVA DAY3/JAVA DAY4/INHERITANCE/College01.java
@@ -0,0 +1,5 @@
+public class College01 extends Student01 
+{
+	private int year;
+	private String major;
+}
---
+++ JAVA DAY3/JAVA DAY4/INHERITANCE/Teacher01.java
@@ -0,0 +1,5 @@
+public class Teacher extends Person2_01
+{
+	private int salary;
+	private String subject;
+}
