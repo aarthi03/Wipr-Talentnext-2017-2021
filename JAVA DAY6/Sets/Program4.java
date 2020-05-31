a857a84706c2117018a14110316ed1dec7646466
Aarthi L <54667558+aarthi03@users.noreply.github.com>
2020-05-31 23:19:28 +0530

    Add files via upload

---
+++ JAVA DAY6/Sets/Program4.java
@@ -0,0 +1,17 @@
+import com.Program1.Country;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		Country countries = new Country();
+		countries.saveCountryNames("India");
+		countries.saveCountryNames("USA");
+		countries.saveCountryNames("Pakistan");
+		countries.saveCountryNames("Bangladesh");
+		countries.saveCountryNames("China");
+
+		System.out.println("China: " + countries.getCountry("China"));
+		System.out.println("Japan: " + countries.getCountry("Japan"));
+	}
+
+}
