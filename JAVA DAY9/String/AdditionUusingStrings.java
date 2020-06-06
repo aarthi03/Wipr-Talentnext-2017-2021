7c71a838f0db416b431c344e60586bb32965942e
Aarthi L <54667558+aarthi03@users.noreply.github.com>
2020-06-07 01:47:11 +0530

    Add files via upload

---
+++ JAVA DAY9/String/AdditionUusingStrings.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import  java.util.*;
+import java.math.BigDecimal;
+
+class AdditionUusingStrings {
+
+	public String addNumberStrings(String input1,String input2){
+		// Read only region end
+		BigDecimal x = new BigDecimal(input1);
+		BigDecimal y = new BigDecimal(input2);
+		
+		return String.valueOf(x.add(y));
+	}
+}
